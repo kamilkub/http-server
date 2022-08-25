47e5604319e182fb48b7fbaa31e27f31b4dc5907
kamilkub <kamil_kubel@o2.pl>
2022-08-25 21:31:29 +0200

    Created enum for keeping constants of HTTP headers

---
+++ src/main/java/org/http/handler/HttpDispatcher.java
@@ -0,0 +1,2 @@
+package org.http.handler;public class HttpDispatcher {
+}
---
+++ src/main/java/org/http/request/HttpStandardHeaders.java
@@ -0,0 +1,50 @@
+package org.http.request;
+
+public enum HttpStandardHeaders {
+
+    AIM("A-IM"),
+    ACCEPT("Accept"),
+    ACCEPT_CHARSET("Accept-Charset"),
+    ACCEPT_ENCODING("Accept-Encoding"),
+    ACCEPT_LANGUAGE("Accept-Language"),
+    ACCEPT_DATETIME("Accept-Datetime"),
+    ACCESS_CONTROL_REQUEST_METHOD("Access-Control-Request-Method"),
+    ACCESS_CONTROL_REQUEST_HEADERS("Access-Control-Request-Headers"),
+    AUTHORIZATION("Authorization"),
+    CACHE_CONTROL("Cache-Control"),
+    CONNECTION("Connection"),
+    CONTENT_LENGTH("Content-Length"),
+    CONTENT_TYPE("Content-Type"),
+    COOKIE("Cookie"),
+    DATE("Date"),
+    EXPECT("Expect"),
+    FORWARDED("Forwarded"),
+    FROM("From"),
+    HOST("Host"),
+    IF_MATCH("If-Match"),
+    IF_MODIFIED_SINCE("If-Modified-Since"),
+    IF_NONE_MATCH("If-None-Match"),
+    IF_RANGE("If-Range"),
+    IF_UNMODIFIED_SINCE("If-Unmodified-Since"),
+    MAX_FORWARDS("Max-Forwards"),
+    ORIGIN("Origin"),
+    PRAGMA("Pragma"),
+    PROXY_AUTHORIZATION("Proxy-Authorization"),
+    RANGE("Range"),
+    REFERER("Referer"),
+    TE("Te"),
+    USER_AGENT("User-Agent"),
+    UPGRADE("Upgrade"),
+    VIA("Via"),
+    WARNING("Warning");
+
+    private final String value;
+
+    HttpStandardHeaders(String value) {
+        this.value = value;
+    }
+
+    public String val() {
+        return this.value;
+    }
+}
