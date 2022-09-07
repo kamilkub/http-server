b49ee182e1fbb1b1e3172a6c3fbb91192cfc34cb
kamilkub <kamil_kubel@o2.pl>
2022-09-07 21:36:49 +0200

    Reading body of request impl

--- src/main/java/org/http/request/reader/HttpBodyReader.java
+++ src/main/java/org/http/request/reader/HttpBodyReader.java
@@ -4,12 +4,17 @@ import org.http.request.parts.Body;
 import org.http.request.parts.RequestPart;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class HttpBodyReader implements HttpReader {
     @Override
     public RequestPart read(List<String> requestLines) {
         Body body = new Body();
-        body.setContent("");
+
+        String [] headersBodySeparator =
+                requestLines.stream().collect(Collectors.joining()).split("\n");
+
+        body.setContent(headersBodySeparator.length > 0 ? headersBodySeparator[1] : "");
 
         return body;
     }
